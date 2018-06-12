9c825e4617324051a7c7bddbc85230a54bc97755
Matt Winzer <matt.winzer@gmail.com>
2018-06-12 12:45:11 -0600

    breakout 1 complete

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# API Intro
+# API Introduction
 
 ## Objectives
 
@@ -10,7 +10,38 @@
 
 ## Notes
 
+- API: Application Programming Interface
+  * A way you can interact with or request data
+  * A way that developers can share their code
+    - Ex. (Wayze, Google Maps)
+  * Software to software interface vs. a user interface
+  * Makes more complex functionality easier to integrate
+    - Hides some complexity (abstraction)
+  * Sending/receiving data to/from different places & perhaps applications
+    - Ex. buying something with a credit card
+  * Internal 
+    * Internal
+      - Fetch
+      - DOM/Document
+    * External
+      - Places where data is held that we can request
+
 ## Resources
 
 - [JSON Placeholder API](https://jsonplaceholder.typicode.com/)
 - [Public APIs](https://github.com/toddmotto/public-apis)
+
+
+# API Introduction Part 2
+
+## Objectives
+
+- Append data from a fetch request to the DOM
+  - Work with an array of data
+- POST data to an API
+
+## Notes
+
+## Resources
+
+- [JSON Placeholder API](https://jsonplaceholder.typicode.com/)
--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-const baseUrl = 'https://jsonplaceholder.typicode.com/posts/'
+var baseUrl = 'https://jsonplaceholder.typicode.com/posts/'
 
 
 // Write a function that fetches data for one blog post from the API
 
+function getBlogPosts(url) {
+  fetch(url)                          // 1. request data
+    .then(function(response) {        // 2. Receive a response
+      return response.json()          // 3. Turn response into JSON so we can work with it
+    })
+    .then(function(data) {
+      // 4. Data is now formatted to JSON so we can do whatever we want
+        // Must do it here because of async
+        // Work with data in some way
+        // Often DOM manipulation
+
+      console.log(data)
+      console.log(data.body)
+      console.log(data.title)
+      
+      // Grab the right section of the DOM to append to
+      var $posts = document.querySelector('.blog-posts')
+
+      // Create a new element
+      var newH1 = document.createElement('h1')
+
+      // Add data to the element
+      newH1.innerText = data.title
+
+      // Append element to page
+      $posts.appendChild(newH1)
+      
+    })
+}
+
+getBlogPosts(baseUrl + '1')
 
 
 // Write a function that appends data from blog post to the page
   // Append to the section with class "posts"
   // title
   // body
-
-// 
 
 
 // Write a function that fetches data for the entire list of blog posts
